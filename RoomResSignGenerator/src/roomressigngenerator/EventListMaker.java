a2c48e03c67bacf34948a62cd562fd767d2e3bbb
Jerry Shaeffer <gps5012@gmail.com>
2012-11-08 21:28:00 -0500

    Update RoomResSignGenerator/src/roomressigngenerator/EventListMaker.java

--- RoomResSignGenerator/src/roomressigngenerator/EventListMaker.java
+++ RoomResSignGenerator/src/roomressigngenerator/EventListMaker.java
@@ -14,21 +14,18 @@ public class EventListMaker
 {
 	private EventSortBox sortBox = new EventSortBox();
 	
-	public EventListMaker()
-	{
+	public EventListMaker() {
 	
 	}
 	
-	public CalendarEvent[] compileList(String urlString) throws Exception
-	{
+	public CalendarEvent[] compileList(String urlString) throws Exception {
 		
 	
 		URL output = new URL(urlString);
 		BufferedReader in = new BufferedReader(new InputStreamReader(output.openStream()));
 		StringBuffer str = new StringBuffer();
 		String line = in.readLine();
-		while(line != null)
-		{
+		while(line != null) {
 			str.append(line);
 			str.append("\n");
 			line = in.readLine();
@@ -42,22 +39,18 @@ public class EventListMaker
 		fullString = fullString.replace("[", "");
 		fullString = fullString.replace("when:", "");
 		String[] eventsArr = fullString.split("kind:calendar#event");
-		for(int i = 2; i < eventsArr.length; i++)
-		{
+		for(int i = 2; i < eventsArr.length; i++) {
 			CalendarEvent event = new CalendarEvent();
 			String eventString = eventsArr[i];
 			String[] eventData = eventString.split(",");
-			for(String eventDatum : eventData)
-			{
-				if(eventDatum.startsWith("title:"))
-				{
+			for(String eventDatum : eventData) {
+				if(eventDatum.startsWith("title:")) {
 					eventDatum = eventDatum.replace("title:", "");
 					String study = eventDatum.substring(0, 3);
 					event.setStudyName(study);
 					event.setEventContact();
 				}
-				else if(eventDatum.startsWith("start:"))
-				{
+				else if(eventDatum.startsWith("start:")) {
 					eventDatum = eventDatum.replace("start:", "");
 					eventDatum = eventDatum.replace(".000-4:00", "");
 
@@ -71,8 +64,7 @@ public class EventListMaker
 					event.setStartTime(startDate);
 					event.setEventDay(dayOutFormat.format(startDate));
 				}
-				else if(eventDatum.startsWith("end:"))
-				{
+				else if(eventDatum.startsWith("end:")) {
 					eventDatum = eventDatum.replace("end:", "");
 					eventDatum = eventDatum.replace(".000-4:00", "");
 					SimpleDateFormat inFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
@@ -90,14 +82,12 @@ public class EventListMaker
 		return eventSet;
 	}
 	
-	public void printEvents(String url) throws Exception
-	{
+	public void printEvents(String url) throws Exception {
 		URL output = new URL(url);
 		BufferedReader in = new BufferedReader(new InputStreamReader(output.openStream()));
 		StringBuffer str = new StringBuffer();
 		String line = in.readLine();
-		while(line != null)
-		{
+		while(line != null) {
 			str.append(line);
 			str.append("\n");
 			line = in.readLine();
@@ -111,25 +101,20 @@ public class EventListMaker
 		fullString = fullString.replace("[", "");
 		fullString = fullString.replace("when:", "");
 		String[] eventsArr = fullString.split("kind:calendar#event");
-		for(int i = 2; i < eventsArr.length; i++)
-		{
+		for(int i = 2; i < eventsArr.length; i++) {
 			String eventString = eventsArr[i];
 			String[] eventData = eventString.split(",");
-			for(String eventDatum : eventData)
-			{
-				if(eventDatum.startsWith("title:"))
-				{
+			for(String eventDatum : eventData) {
+				if(eventDatum.startsWith("title:")) {
 					eventDatum = eventDatum.replace("title:", "");
 					System.out.println(eventDatum);
 				}
-				else if(eventDatum.startsWith("start:"))
-				{
+				else if(eventDatum.startsWith("start:")) {
 					eventDatum = eventDatum.replace("start:", "");
 					eventDatum = eventDatum.replace(".000-4:00", "");
 					System.out.println(eventDatum);
 				}
-				else if(eventDatum.startsWith("end:"))
-				{
+				else if(eventDatum.startsWith("end:")) {
 					eventDatum = eventDatum.replace("end:", "");
 					eventDatum = eventDatum.replace(".000-4:00", "");
 					System.out.println(eventDatum);		
